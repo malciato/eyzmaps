171a999220adbaaf4459875ad078c6033390215e
D See Ker <dseeker@users.noreply.github.com>
2019-04-09 18:29:20 +0200

    require libraries

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 const fs = require('fs')
 const p = require('path')
 const maps = {}
+const folder = p.join(__dirname,'maps')
 
-
-var files = fs.readdirSync(p.join(__dirname,'maps')).map(f => {
+var files = fs.readdirSync(folder).map(f => {
   try {
-      return fs.readFileSync(p.join(dir,f), 'utf8')
-        .split('\n')
-        // .filter(v => v.indexOf('##')!=0 && v.indexOf(' completed')>0 && regExp.exec(v))
-        .map(v=> p.dirname(v))
+      maps[p.basename(f)] = require(p.join(folder,f))
   } catch(e) {
       console.log('Error:', e.stack);
   }
 })
 
-maps.files = files
-
 exports = maps
-// exports.printMsg = function() {
-//   console.log("This is a message from the demo package", files);
-// }
