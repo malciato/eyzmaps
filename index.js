f9b2f7aa0308f5a7681e32b775f89ab5e82bc378
D See Ker <dseeker@users.noreply.github.com>
2019-04-11 23:30:30 +0200

    fix webpack import warning

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const folder = p.join(__dirname,'maps')
 
 var files = fs.readdirSync(folder).map(f => {
   try {
-      maps[p.basename(f, p.extname(f))] = require(p.join(folder,f))
+      maps[p.basename(f, p.extname(f))] = require(`${p.join(folder,f)}`)
   } catch(e) {
       console.log('Error:', e.stack);
   }
